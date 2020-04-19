cc0a2420366e54e60d91a090a13ad1d3bb54dccb
karneol10 <karneoll12@gmail.com>
2020-04-19 17:21:51 +0200

    Add xamarin hello world

---
+++ Xamarin/hello_world/XamarinCross/XamarinCross/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinCross
+{
+    public partial class MainPage : ContentPage
+    {
+        Button ChangeText;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PressMeButton_Pressed(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Nie naciskaj na mnie bardziej!";
+        }
+
+        private void PressMeButton_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Właśnie zostałem naciśnion!";
+        }
+
+    }
+}
